a5d282d9254586ee20b64334aabc96efa871f1b4
Yuto Miura <veda.ym0209@gmail.com>
2019-04-01 14:48:33 -0700

    read until day-07

--- day-05/src/Content3.js
+++ day-05/src/Content3.js
@@ -4,8 +4,8 @@ import ActivityItem from './ActivityItem';
 
 class Content extends React.Component {
   render() {
-    const {activities} = this.props; // ES6 destructuring
-    
+    const { activities } = this.props; // ES6 destructuring
+
     return (
       <div className="content">
         <div className="line"></div>
@@ -15,7 +15,7 @@ class Content extends React.Component {
           <ActivityItem
             activity={activity} />
         ))}
-        
+
       </div>
     )
   }
